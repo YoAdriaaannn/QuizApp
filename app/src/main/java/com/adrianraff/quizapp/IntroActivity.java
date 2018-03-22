0dfb4f6ba54e26489f0f3d84cb88a201960ef683
fr0stsp1re <info@atomiccomputerservice.com>
2018-03-21 17:10:49 -0700

    WIP UI

--- app/src/main/java/com/adrianraff/quizapp/IntroActivity.java
+++ app/src/main/java/com/adrianraff/quizapp/IntroActivity.java
@@ -1,13 +1,27 @@
 package com.adrianraff.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class IntroActivity extends AppCompatActivity {
+
+
+    /**
+     *
+     * Set up variables to be used app wide starting with user name, score, total right and total wrong aswers
+     */
+
+     EditText userName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+    }
+
+    public void startApp(){
+        userName = findViewById(R.id.editText_user_name);
     }
 }
